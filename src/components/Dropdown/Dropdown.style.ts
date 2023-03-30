08cc2091c8fe392b20d7f73470ac6dd3f0263ace
Christopher T Wilson <vyper375@hotmail.com>
2023-03-30 16:37:21 +0100

    Adjusted Dropdown component to add optional groups and also fix styling issues. Added prevention of invalid values.

--- src/components/Dropdown/Dropdown.style.ts
+++ src/components/Dropdown/Dropdown.style.ts
@@ -2,19 +2,25 @@ import styled from "styled-components";
 import StyledInput from "../Input/Input.style";
 
 const StyledDropdown = styled(StyledInput)`
-  	&::after {
-        content: '';
-        position: absolute;
-        top: calc(50% - 2px);
-        right: 8px;
-        width: 6px;
-        height: 6px;
-        transform: translate(0px, -50%) rotate(45deg);
-        border: 1px solid black;
-        border-top: none;
-        border-left: none;
-        pointer-events: none;
-    }
+	& > .dropdown {
+		position: relative;
+		width: ${props => props.width};
+
+		&::after {
+		  content: '';
+		  position: absolute;
+		  top: calc(50% - 2px);
+		  right: 8px;
+		  width: 6px;
+		  height: 6px;
+		  transform: translate(0px, -50%) rotate(45deg);
+		  border: 1px solid var(--text-color);
+		  border-top: none;
+		  border-left: none;
+		  pointer-events: none;
+	  }
+	}
+
 `;
 
 export default StyledDropdown
--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -2,28 +2,44 @@ import StyledDropdown from "./Dropdown.style";
 import StyledInputLabel from "../styled/InputLabel";
 
 type DropdownProps = {
-	value: string | number;
-	options: Array<{value: string | number, label: string}>;
-	onChange: (value: string | number) => void;
+	value: string | number | undefined;
+	options: Array<{value: string | number, label: string, options?: Array<{value: string | number, label: string}>}>;
+	onChange: (value: string) => void;
 	label?: string;
 	width?: string;
 	labelWidth?: string;
 }
 
-const Dropdown = ({value, options, onChange, label, width='100%', labelWidth='auto' }: DropdownProps) => {
+const Dropdown = ({value, label, onChange, width='100%', labelWidth='auto', options}: DropdownProps) => {
 	const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
 		onChange(event.target.value);
 	}
 
+	if (value === 0 || value === undefined || Number.isNaN(value)) value = '';
+
 	return (
 		<StyledDropdown width={width}>
 			<StyledInputLabel width={labelWidth}>{label}</StyledInputLabel>
-			<select value={value} onChange={handleChange} aria-label={label}>
-				<option value="" disabled>Select an option</option>
-				{options.map((option) => (
-					<option key={option.value} value={option.value}>{option.label}</option>
-				))}
-			</select>
+			<div className='dropdown'>
+				<select value={value} onChange={handleChange} aria-label={label}>
+					<option value="" disabled>Select an option</option>
+					{options.map((option) => {
+						if (option.options) {
+							return (
+								<optgroup label={option.label} key={option.label}>
+									{option.options.map((subOption) => (
+										<option key={subOption.value} value={subOption.value}>{subOption.label}</option>
+									))}
+								</optgroup>
+							)
+						} else {
+							return (
+								<option key={option.value} value={option.value}>{option.label}</option>
+							)
+						}
+					})}
+				</select>
+			</div>
 		</StyledDropdown>
 	);
 }
