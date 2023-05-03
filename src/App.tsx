b06a3e9da7bca5becec2b6a61f013b2dd738e724
Christopher T Wilson <vyper375@hotmail.com>
2023-05-03 19:26:48 +0100

    Added Categories page to App

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { setAddingTransaction, selectTransaction } from './redux/transactionsSli
 import MenuBar from './components/MenuBar/MenuBar';
 import Home from './pages/Home/Home';
 import Accounts from './pages/Accounts/Accounts';
+import Categories from './pages/Categories/Categories';
 import Modal from './components/Modal/Modal';
 import TransactionForm from './components/TransactionForm/TransactionForm';
 
@@ -26,6 +27,7 @@ function App() {
 		<div className="App">
 			<MenuBar/>
 			{ currentPage === 'Home' ? <Home/> : null }
+			{ currentPage === 'Categories' ? <Categories/> : null }
 			{ currentPage === 'Accounts' ? <Accounts/> : null }
 			{ addingTransaction ? <Modal heading='Add Transaction' onClickClose={onCloseModal}><TransactionForm/></Modal> : null }
 			{ selectedTransaction ? <Modal heading='Edit Transaction' onClickClose={onCloseModal} x={centerX-150} y={centerY-150}><TransactionForm key={selectedTransaction.id} obj={selectedTransaction}/></Modal> : null }
