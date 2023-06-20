bf9245d1402643433b794470e840f63ccd5f3033
Christopher T Wilson <vyper375@hotmail.com>
2023-06-20 15:01:09 +0100

    Fixed IconButton color defaulting to white instead of global variable.

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -11,7 +11,7 @@ type IconButtonProps = {
 	[key: string]: any;
 }
 
-const IconButton = ({Icon, fontSize='1em', color='white', onClick, cursor='pointer', ...props}: IconButtonProps) => {
+const IconButton = ({Icon, fontSize='1em', color='var(--text-color)', onClick, cursor='pointer', ...props}: IconButtonProps) => {
 	return (
 		<StyledIconButton onClick={onClick} cursor={cursor} {...props}>
 			<Icon style={{fontSize, color}}/>
